0fa18ed3bb1dd3b57638a8578f8fc09829af34bc
Byren Cheema <bscheema@uci.edu>
2024-08-04 15:51:25 -0700

    Basic Inventory System

--- app/page.js
+++ app/page.js
@@ -1,6 +1,83 @@
+'use client';
 import Image from "next/image";
+import { useState, useEffect } from "react";
+import { firestore } from "../firebase";
+import {Box, Modal, Typography, Stack, TextField} from "@mui/material";
+import { collection, setDoc } from "firebase/firestore";
+import { getDocs, query } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(true);
+  const [itemName, setItemName] = useState("");
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = []
+    docs.forEach(doc => {
+      inventoryList.push({ name: doc.id,
+        ...doc.data() });
+    });
+
+    setInventory(inventoryList);
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(firestore, "inventory", item.name);
+    const docSnap = await getDocs(docRef);
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      }
+      else {
+        await setDoc(docRef, {quantity: quantity - 1});
+      }
+    }
+
+    awaitupdateInventory();
+  }
+
+  const addItem = async () => {
+    const docRef = doc(firestore, "inventory", itemName);
+    const docSnap = await getDocs(docRef);
+
+    if (docSnap.exists()) {
+      const {quantity} = docSnap.data();
+      await setDoc(docRef, {quantity: quantity + 1});
+    }
+    else {
+      await setDoc(docRef, {quantity: 1});
+    }
+
+    await updateInventory();
+  }
+
+  const handleOpen = () => {
+    setOpen(true);
+  }
+  
+  const handleClose = () => {
+    setOpen(false);
+  }
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
   return (
-  <> Hello World </>);
+  <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" gap={2}>
+    <Modal open={open} onClose={handleClose}>
+      <Box position="absolute" top='50%' left='50%' bgcolor="white" p={2} borderRadius={2} width={400} border="2px solid #00000" p={4} display="flex" flexDirection={"column"} gap={3} sx={{transform: 'translate(-50%, -50%)'}}>
+        <Typography variant="h6">Add Item</Typography>
+        <Stack width="100%" direction="row" spacing={2}>
+          <TextField></TextField>
+        </Stack>
+      </Box>
+    </Modal>
+    <Typography variant="h1">Inventory Manager</Typography>
+  </Box>
+  )
 }
